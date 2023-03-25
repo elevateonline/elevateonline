525897f2bcc2cf3cf790042f0126dd6886da2d5b
elevateonline <elevateweb2k23@gmail.com>
2023-03-25 11:18:39 +0530

    added firebase authentication

--- login/index.js
+++ login/index.js
@@ -7,3 +7,26 @@ registerlink.addEventListener('click',()=>{
 loginlink.addEventListener('click',()=>{
     loginsec.classList.remove('active')
 })
+
+
+const signInWithGoogleButton = document.getElementById('signInWithGoogle');
+
+//Necessary part for the firebase built in functions
+//It's easier and cleaner to type auth.signInWithEmailAndPassword
+//than firebase.auth().signInWithEmailAndPassword
+//also it's less repetitive since we are using it more than once
+const auth = firebase.auth();
+
+const signInWithGoogle = () => {
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+  
+    auth.signInWithPopup(googleProvider)
+    .then(() => {
+      window.location.assign('./profile');
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }
+  
+  signInWithGoogleButton.addEventListener('click', signInWithGoogle);
